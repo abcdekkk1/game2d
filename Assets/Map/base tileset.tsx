d652be8b9c778d34181e4f4b7d008bda40cf2237
abcdekkk1 <49889097+abcdekkk1@users.noreply.github.com>
2019-10-15 11:54:03 +0700

    dsf
    dfdsf

---
+++ Assets/Map/base tileset.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="base tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="1620" columns="54">
+ <image source="D:/music/GamesPlusJames - Unity 2D RPG/preview-medals.png" width="918" height="515"/>
+ <tile id="278">
+  <animation>
+   <frame tileid="224" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
